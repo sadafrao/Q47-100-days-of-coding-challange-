7ae28bd9089ab45226f41cebd8156ec8d581b650
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 17:08:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//           Q 47
+// Advanec array use array and put different properties of object inside an array 
+// add first and second to variables then log this variables
+var laptops = [
+    {
+        make: "Dell",
+        model: "Xps",
+        year: "2021",
+    },
+    {
+        make: "Apple",
+        model: "mac book",
+        year: "2024",
+    },
+];
+var laptop1 = laptops[0], laptop2 = laptops[1];
+console.log(laptops);
---
+++ index.ts
@@ -0,0 +1,19 @@
+//           Q 47
+// Advanec array use array and put different properties of object inside an array 
+// add first and second to variables then log this variables
+
+let laptops = [
+    {
+        make : "Dell",
+        model : "Xps",
+        year : "2021",
+},
+    {
+        make : "Apple",
+        model : "mac book",
+        year : "2024",
+},
+
+];
+// let [laptop1 , laptop2] = laptops
+console.log(laptops);
